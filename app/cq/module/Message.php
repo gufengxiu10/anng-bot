80fd53b4ea86453b9153e39f1cbea07d16f5e873
anng <annghanyuu@gmail.com>
2021-03-16 17:58:25 +0800

    修改成异步写法

--- app/cq/module/Message.php
+++ app/cq/module/Message.php
@@ -4,6 +4,32 @@ declare(strict_types=1);
 
 namespace app\cq\module;
 
+use app\cq\Cq;
+
 class Message
 {
+    public function sendPrivate(string $userId, string $groudId, string|Cq $message): string
+    {
+        return $this->set('send_private_msg', [
+            'user_id' => $userId,
+            'group_id' => $groudId,
+            'message' => $message
+        ]);
+    }
+
+    public function sendGroup(string $groudId, string|Cq $message): string
+    {
+        return $this->set('send_private_msg', [
+            'group_id' => $groudId,
+            'message' => $message
+        ]);
+    }
+
+    public function set(string $uri, $params): string
+    {
+        return json_encode([
+            'action' => $uri,
+            'params' => $params
+        ]);
+    }
 }
